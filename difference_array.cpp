93de6f9f16830a825a3997d36f2d25adb60e63a1
Antor Sarker <antor.s.cse10.bu@gmail.com>
2025-08-25 00:03:41 +0600

    Create difference_array.cpp

---
+++ difference_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    int dif[n+1]={0};
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r,v;
+        cin>>l>>r>>v;
+        dif[l]+=v;
+        dif[r+1]-=v;
+    }
+    for(int i=1;i<n;i++)dif[i]+=dif[i-1];
+    for(int i=0;i<n;i++)arr[i]+=dif[i];
+    for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+    cout<<endl;
+    return 0;
+}
