17e929b4c7ea3564b41bee65d7255e57efdc757e
Antor Sarker <antor.s.cse10.bu@gmail.com>
2025-08-26 19:41:35 +0600

    Create all_subarray_sum.cpp

---
+++ all_subarray_sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve(int arr[],int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i*(n-i+1)*arr[i];
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for(int i=1;i<=n;i++)cin>>arr[i];
+    cout<<solve(arr,n)<<endl;
+    return 0;
+}
